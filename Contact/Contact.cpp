187f2ce6e8f3f5dbd9f9207348efa6e3730e22ba
Jonathan Godbout <jon.godbout@gmail.com>
2019-01-25 00:10:26 -0500

    Work in progress of Visualizer class
    - defined Cloud class, can add generic features to it
    - next, write PCD file

--- Contact/Contact.cpp
+++ Contact/Contact.cpp
@@ -1,6 +1,8 @@
 #include "stdafx.h"
 #include <stdlib.h>
 
+#include <algorithm>
+#include <map>
 #include <string>
 #include <vector>
 
@@ -14,20 +16,68 @@ class Visualizer : public pcl::visualization::PCLVisualizer
 public:
 	Visualizer(const std::string& name, int nbRows = 1, int nbCols = 1);
 
+	bool hasCloud(const std::string& name) { return mClouds.find(name) != std::end(mClouds); }
+
+	using CloudName = std::string;
+	using FeatureName = std::string;
+	using FeatureData = std::vector<float>;
+
 	class Cloud
 	{
 	public:
 		Cloud() = default;
 
-	private:
+		void add(const FeatureData& data, const FeatureName& name)
+		{
+			// assert size if > 0
+			mFeatures[name] = data;
+		}
 
+	private:
+		std::map<FeatureName, FeatureData> mFeatures;
 	};
+
+	Cloud& add(const pcl::PointCloud<pcl::PointXYZ>& data, const CloudName& name)
+	{
+		const int nbPoints = data.size();
+		auto& cloud = mClouds[name];
+
+		// TODO: must generalize this code.
+		FeatureData values(nbPoints);
+		std::transform(std::begin(data), std::end(data), std::begin(values), [](const pcl::PointXYZ& p) { return p.x; });
+		cloud.add(values, "x");
+		std::transform(std::begin(data), std::end(data), std::begin(values), [](const pcl::PointXYZ& p) { return p.y; });
+		cloud.add(values, "y");
+		std::transform(std::begin(data), std::end(data), std::begin(values), [](const pcl::PointXYZ& p) { return p.z; });
+		cloud.add(values, "z");
+
+		return cloud;
+	}
+
+	Cloud& add(const FeatureData& data, const FeatureName& featName, const CloudName& cloudName)
+	{
+		const int nbPoints = data.size();
+		auto& cloud = mClouds[cloudName];
+		cloud.add(data, featName);
+		return cloud;
+	}
+
+private:
+	std::map<CloudName, Cloud> mClouds;
+
+	std::vector<int> mViewportIds;
 };
 
 Visualizer::Visualizer(const std::string& name, int nbRows, int nbCols) :
 	pcl::visualization::PCLVisualizer(name)
 {
-
+	mViewportIds.resize(nbRows * nbCols);
+	const float sizeX = 1 / nbCols;
+	const float sizeY = 1 / nbRows;
+	int k = 0;
+	for (int i = 0; i < nbCols; ++i)
+		for (int j = 0; j < nbRows; ++j)
+			createViewPort(i*sizeX, j*sizeY, (i + 1)*sizeX, (j + 1)*sizeY, mViewportIds[k++]);
 }
 
 int main()
@@ -44,11 +94,16 @@ int main()
 		}
 	}
 
+	std::vector<float> idx(cloud->size());
+	for (int i = 0; i < (int)cloud->size(); ++i)
+		idx[i] = (float)i;
+
 	std::cout << *cloud << std::endl;
 	pcl::io::savePCDFile("cloud.pcd", *cloud);
 
 	Visualizer viewer("A cloud");
-	viewer.addPointCloud(cloud, "random-cloud");
+	viewer.add(*cloud, "random-cloud");
+	viewer.add(idx, "index", "random-cloud");
 
 	viewer.spin();
 
