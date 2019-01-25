1c2ccda8f3247035fbc57f4acb2fcb67ca9d6533
Jonathan Godbout <jon.godbout@gmail.com>
2019-01-24 22:30:36 -0500

    Added visualization
    - and applied camelCase coding standard

--- Contact/Contact.cpp
+++ Contact/Contact.cpp
@@ -3,23 +3,29 @@
 #include <pcl/pcl_base.h>
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
 int main()
 {
 	using CloudType = pcl::PointCloud<pcl::PointXYZ>;
-	CloudType::Ptr Cloud (new CloudType());
+	CloudType::Ptr cloud (new CloudType());
 
 	for (float x = 0.0; x < 1.0; x += 0.01)
 	{
 		for (float y = 0.0; y < 1.0; y += 0.01)
 		{
 			const float z = rand() / 100000.f;
-			Cloud->push_back({ x,y,z });
+			cloud->push_back({ x,y,z });
 		}
 	}
 
-	std::cout << *Cloud << std::endl;
-	pcl::io::savePCDFile("cloud.pcd", *Cloud);
+	std::cout << *cloud << std::endl;
+	pcl::io::savePCDFile("cloud.pcd", *cloud);
+
+	pcl::visualization::PCLVisualizer viewer("A cloud");
+	viewer.addPointCloud(cloud, "random-cloud");
+
+	viewer.spin();
 
     return 0;
 }
