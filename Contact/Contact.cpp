19a307d4232e1f76ca54a795200aa4a685016271
Jonathan Godbout <jgodbout@bathfitter.com>
2019-01-28 07:41:31 -0500

    Use spaces not tabs

--- Contact/Contact.cpp
+++ Contact/Contact.cpp
@@ -20,45 +20,45 @@ float randf() { return rand() / static_cast<float>(RAND_MAX); }
 
 PointsType::Ptr makeCloud()
 {
-	PointsType::Ptr cloud(new PointsType());
+    PointsType::Ptr cloud(new PointsType());
 
-	for (float x = 0.0; x < 1.0; x += 0.01)
-	{
-		for (float y = 0.0; y < 1.0; y += 0.01)
-		{
-			// The offset is to make sure to have consistent normals directions.
-			const float z = 3.0 + 0.1 * std::sin(x*M_PI*5.0) * std::sin(y*M_PI*3.0);
-			cloud->push_back({ x, y, z });
-		}
-	}
+    for (float x = 0.0; x < 1.0; x += 0.01)
+    {
+        for (float y = 0.0; y < 1.0; y += 0.01)
+        {
+            // The offset is to make sure to have consistent normals directions.
+            const float z = 3.0 + 0.1 * std::sin(x*M_PI*5.0) * std::sin(y*M_PI*3.0);
+            cloud->push_back({ x, y, z });
+        }
+    }
 
-	return cloud;
+    return cloud;
 }
 
 int main()
 {
-	PointsType::Ptr cloudModel = makeCloud();
+    PointsType::Ptr cloudModel = makeCloud();
 
-	// Noisy cloud.
-	PointsType::Ptr cloudNoisy = makeCloud();
-	for (auto& p : cloudNoisy->points)
-		p.z += 0.05*randf();
+    // Noisy cloud.
+    PointsType::Ptr cloudNoisy = makeCloud();
+    for (auto& p : cloudNoisy->points)
+        p.z += 0.05*randf();
 
-	// Transformed cloud.
-	PointsType::Ptr cloudMoved (new PointsType());
-	Eigen::Affine3f T = Eigen::Affine3f::Identity();
-	T.translate(Eigen::Vector3f(0.05, 0, 0));
-	pcl::transformPointCloud(*cloudModel, *cloudMoved, T);
+    // Transformed cloud.
+    PointsType::Ptr cloudMoved (new PointsType());
+    Eigen::Affine3f T = Eigen::Affine3f::Identity();
+    T.translate(Eigen::Vector3f(0.05, 0, 0));
+    pcl::transformPointCloud(*cloudModel, *cloudMoved, T);
 
-	// Normals.
+    // Normals.
     using NormalsType = pcl::PointCloud<pcl::Normal>;
     NormalsType::Ptr normals(new NormalsType());
-	pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-	ne.setInputCloud(cloudModel);
-	ne.setKSearch(30);
-	ne.compute(*normals);
+    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+    ne.setInputCloud(cloudModel);
+    ne.setKSearch(30);
+    ne.compute(*normals);
 
-	// Some array features.
+    // Some array features.
     std::vector<float> idx(cloudModel->size());
     std::vector<float> rnd(cloudModel->size());
     for (int i = 0; i < (int)cloudModel->size(); ++i)
@@ -70,48 +70,48 @@ int main()
     std::cout << *cloudModel << std::endl;
     pcl::io::savePCDFile("cloud.pcd", *cloudModel);
 
-	auto multipleViewports = [&]()
-	{
-		VISUALIZER_CALL(Visualizer viewer("multiple-viewports", 2, 3));
+    auto multipleViewports = [&]()
+    {
+        VISUALIZER_CALL(Visualizer viewer("multiple-viewports", 2, 3));
 
-		std::string NAME;
-		int viewport = 0;
+        std::string NAME;
+        int viewport = 0;
 
-		NAME = "add-feature-multiplecalls"; 
-		VISUALIZER_CALL(viewer.add(*cloudModel, NAME));
-		VISUALIZER_CALL(viewer.addFeature(idx, "index", NAME));
-		viewport++;
+        NAME = "add-feature-multiplecalls"; 
+        VISUALIZER_CALL(viewer.add(*cloudModel, NAME));
+        VISUALIZER_CALL(viewer.addFeature(idx, "index", NAME));
+        viewport++;
 
-		NAME = "add-feature-chained";
-		VISUALIZER_CALL(viewer.add(*cloudModel, NAME, viewport).addFeature(rnd, "randv").addFeature(idx, "index"));
-		viewport++;
+        NAME = "add-feature-chained";
+        VISUALIZER_CALL(viewer.add(*cloudModel, NAME, viewport).addFeature(rnd, "randv").addFeature(idx, "index"));
+        viewport++;
 
-		NAME = "add-feature-cloud-to-cloud-multiplecalls";
-		VISUALIZER_CALL(viewer.add(*cloudModel, NAME, viewport).addFeature(rnd, "randv"));
-		VISUALIZER_CALL(viewer.add(*normals, NAME, viewport));
-		viewport++;
+        NAME = "add-feature-cloud-to-cloud-multiplecalls";
+        VISUALIZER_CALL(viewer.add(*cloudModel, NAME, viewport).addFeature(rnd, "randv"));
+        VISUALIZER_CALL(viewer.add(*normals, NAME, viewport));
+        viewport++;
 
-		NAME = "add-feature-lamdba-to-cloud-chained";
-		VISUALIZER_CALL(viewer.add(*cloudModel, NAME, viewport)
-			.addFeature(normals->points, "c", [](const pcl::Normal& p) { return p.curvature; }));
-		viewport++;
+        NAME = "add-feature-lamdba-to-cloud-chained";
+        VISUALIZER_CALL(viewer.add(*cloudModel, NAME, viewport)
+            .addFeature(normals->points, "c", [](const pcl::Normal& p) { return p.curvature; }));
+        viewport++;
 
-		NAME = "add-feature-lamdba-to-cloud-multiplecalls";
-		VISUALIZER_CALL(viewer.addFeature(cloudModel->points, "x", NAME, [](const pcl::PointXYZ& p) { return p.x; }, viewport));
-		VISUALIZER_CALL(viewer.addFeature(cloudModel->points, "y", NAME, [](const pcl::PointXYZ& p) { return p.y; }, viewport));
-		VISUALIZER_CALL(viewer.addFeature(cloudModel->points, "z", NAME, [](const pcl::PointXYZ& p) { return p.z; }, viewport));
-		viewport++;
+        NAME = "add-feature-lamdba-to-cloud-multiplecalls";
+        VISUALIZER_CALL(viewer.addFeature(cloudModel->points, "x", NAME, [](const pcl::PointXYZ& p) { return p.x; }, viewport));
+        VISUALIZER_CALL(viewer.addFeature(cloudModel->points, "y", NAME, [](const pcl::PointXYZ& p) { return p.y; }, viewport));
+        VISUALIZER_CALL(viewer.addFeature(cloudModel->points, "z", NAME, [](const pcl::PointXYZ& p) { return p.z; }, viewport));
+        viewport++;
 
-		NAME = "multiple-clouds-properties-multiplecalls";
-		VISUALIZER_CALL(viewer.add(*cloudModel, NAME + "-0", viewport).addFeature(idx, "index").add(*normals).setColor(1.0, 0.0, 0.0).setOpacity(0.5).setSize(5));
-		VISUALIZER_CALL(viewer.add(*cloudNoisy, NAME + "-1", viewport));
-		viewport++;
+        NAME = "multiple-clouds-properties-multiplecalls";
+        VISUALIZER_CALL(viewer.add(*cloudModel, NAME + "-0", viewport).addFeature(idx, "index").add(*normals).setColor(1.0, 0.0, 0.0).setOpacity(0.5).setSize(5));
+        VISUALIZER_CALL(viewer.add(*cloudNoisy, NAME + "-1", viewport));
+        viewport++;
 
-		VISUALIZER_CALL(viewer.render());
-	};
+        VISUALIZER_CALL(viewer.render());
+    };
 
-	//
-	multipleViewports();
+    //
+    multipleViewports();
  
     return 0;
 }
